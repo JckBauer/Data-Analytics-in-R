c452c0126a68e6d16b6219dd5b2da72341bfc68a
JckBauer <businessjgcb@gmail.com>
2022-12-22 23:50:36 -0800

    Data Wrangling Experiment on Branching  #Please enter the commit message for your changes. Lines starting

--- Flight-Price-Analysis/Flight-Price-Exploration.R
+++ Flight-Price-Analysis/Flight-Price-Exploration.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(lubridate)
 getwd()
 dir()
-setwd("C:/Users/hellm/Desktop/Projects/r_projects/Data-Analytics-in-R/Flight Price Analysis")
+setwd("C:/Users/hellm/Desktop/Projects/r_projects/Data-Analytics-in-R/Flight-Price-Analysis")
 df = read.csv("Clean_Dataset.csv")
 business = read.csv("business.csv")
 economy = read.csv("economy.csv")
@@ -55,7 +55,8 @@ business_clean = business %>%
             trimws() %>%
             as_factor(),
         time_taken_sec = (
-            hour(parse_time(time_taken, format = "%hh %Mm")) * 3600 + 
+            day(parse_time(time_taken, format = "%hh %Mm")) * 60 * 60 * 24 +
+            hour(parse_time(time_taken, format = "%hh %Mm")) * 60 * 60 + 
             minute(parse_time(time_taken, format = "%hh %Mm")) * 60),
         time_taken = period(second = time_taken_sec),
         flight_code = str_c(ch_code, "-", as.character(num_code))) %>%
@@ -98,7 +99,8 @@ economy_clean = economy_edit %>%
             trimws() %>%
             as_factor(),
         time_taken_sec = (
-            hour(parse_time(time_taken, format = "%hh %Mm")) * 3600 + 
+            day(parse_time(time_taken, format = "%hh %Mm")) * 60 * 60 * 24 +
+            hour(parse_time(time_taken, format = "%hh %Mm")) * 60 * 60 + 
             minute(parse_time(time_taken, format = "%hh %Mm")) * 60),
         time_taken = period(second = time_taken_sec),
         flight_code = str_c(ch_code, "-", as.character(num_code))) %>%
@@ -108,41 +110,53 @@ economy_clean = economy_edit %>%
     mutate(class = "economy")
 }
 
-#checking 
-{
-unique(business_clean$to)
-glimpse(business_clean)
-summary(business_clean)
-unique(business_clean$flight_code)
-}
-
 #Joining the sets
 {
-
+flights = full_join(economy_clean, business_clean)  %>%
+    mutate(class = as_factor(class))
 }
+
+#checking 
+{
+summary(flights)
+nrow(flights)
+nrow(df)
+}
+
+flights %>% 
+    group_by(date, flight_code, dep_time) %>%
+    count() %>%
+    arrange(desc(n))
+vignette(package = 'dplyr')
 
 #plot to see predictions to be made
 {
-business_AI %>%
-    ggplot(aes(duration, price)) +
-    geom_point(aes(color = class)) +
-    geom_smooth(color = class)
+df %>%
+    ggplot(aes(duration, price, color = class)) +
+        geom_point(alpha = 0.01) +
+        geom_smooth()
 }
+
+
+# Work to illuminate how duration == 0 was occurring
+{
+    alltimes = unique(business$time_taken)
+    alltimes
+    index2 = str_detect(business$time_taken, "24h 00m")
+    nrow(filter(tibble(business$time_taken), index2))
+    nrow(business)
+    nrow(filter(business_clean, duration <= 0))
+}
+
 #extras
 {
-    #clean up
-    df = df %>% 
-        rename(rownum = X) %>%
-        glimpse()
-    
-    #Filter to test how the data structures line-up
-    business_AI = business %>%
-        filter(airline == "Air India")
-    
-    df_AI = df %>%
-        filter(airline == "Air_India" & class == "Business")
-    
-    glimpse(business_clean)
-    glimpse(df_AI)
-    
+#Filter to test how the data structures line-up
+business_AI = business %>%
+    filter(airline == "Air India")
+
+df_AI = df %>%
+    filter(airline == "Air_India" & class == "Business")
+
+glimpse(business_clean)
+glimpse(df_AI)
 }
